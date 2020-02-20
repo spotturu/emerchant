8861d0b83bab3e223170ee17853dcd69a5c29887
vasu <sreenivas2reach@gmail.com>
2020-02-20 23:07:00 +0100

    STI and factory desgin added

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,26 @@
 class Transaction < ApplicationRecord
-belongs_to :merchant
+	belongs_to :merchant
+	acts_as_list scope: :merchant
+	scope transactions_by ->(type) do
+		where(status_type: type)
+	end
+end
+
+class Refund < Transaction
+	validates :parent_id, presence: true
+	scope transactions -> do
+		transactions_by('Refund')
+	end
+end
+
+class Cancel < Transaction
+	scope transactions -> do
+		transactions_by('Cancel')
+	end
+end
+
+class Success < Transaction
+	scope transactions -> do
+		transactions_by('Success')
+	end
 end
